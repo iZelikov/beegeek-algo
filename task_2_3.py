b2536ffe336bb9006e4aa03c37c3b037491d92e8
iZelikov <ivan@zelikov.name>
2025-11-05 21:03:17 +0300

    finish 2_3

--- task_2_3.py
+++ task_2_3.py
@@ -10,19 +10,25 @@ def get_quadrant(p):
         elif p[1] < 0:
             return 3
 
+
 def is_point_in_rectangle(p, rect):
     return rect[0][0] < p[0] < rect[1][0] and rect[0][1] < p[1] < rect[1][1]
 
 
 def draw_graph(f):
     n = 9
-    matrix = [['   ' for _ in range(n)] for _ in range(n)]
-    for x in range(1, n+1):
-        y = f(x)
-        if 1 <= y <= n:
-            print(x, y)
-            matrix[y][x] = '  *'
-
-    print(*matrix, sep='\n')
-
-draw_graph(lambda x: x)
+    width = 3
+    n = n + 1
+    underline = f'{'+':>{width}}' + '-' * width * (n - 1) + ' > x'
+    abscissas = ' ' * width + ''.join([f'{i:>{width}}' for i in range(1, n)])
+    matrix = [[' '*width for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        matrix[i][0] = f'{(n - i):<{width-1}}|'
+        if i > 0:
+            y = f(i)
+            if 1 <= y < n:
+                matrix[n - y][i] = f'{"*":>{width}}'
+    matrix[0][0] = 'y ^'
+    [print(*line, sep='') for line in matrix]
+    print(underline)
+    print(abscissas)
