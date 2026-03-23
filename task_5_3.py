5f3aed203f63824161e2220c81ed935efb9616d5
iZelikov <ivan@zelikov.name>
2026-03-23 18:15:53 +0300

    finish 5_3

--- task_5_3.py
+++ task_5_3.py
@@ -1,3 +1,31 @@
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
+def fizz_buzz(n: int) -> tuple[int, int, int]:
+    a = (n - 1) // 3
+    b = (n - 1) // 5
+    c = (n - 1) // 15
+    return a - c, b - c, c
+
+
+def calculate_sum(n: int) -> float:
+    return 1 - 1 / n
+
+
+def calculate_sum(n: int) -> float:
+    return n * (n + 1) // 2
+
+
+def max_consecutive_elements(s: str):
+    max_l = 0
+    l = 0
+    last_c = None
+    for c in s:
+        if last_c == c:
+            l += 1
+        else:
+            last_c = c
+            max_l = max(max_l, l)
+            l = 1
+    return max(max_l, l)
+
+
+def water_requirement(n: int) -> int:
+    return n * (n + 1) + 1
